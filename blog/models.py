38eb952021ac3ab53628e7fdaae107d2a19b29d8
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2024-09-10 09:22:33 +0000

    add category model and migrate it

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,16 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"  
+        ordering = ['name']   
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title=models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -12,9 +22,21 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"The title of this post is {self.title} by {self.author}"    
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"   
