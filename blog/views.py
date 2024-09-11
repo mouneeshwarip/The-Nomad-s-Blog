8c18205808a2af4236dff76f41271730e2cf9159
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2024-09-11 21:45:06 +0000

    add like and share count annotations to PostList view

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Post
+from django.db.models import Count
+from .models import Post, Like, Share
 
 # Create your views here.
+
 class PostList(generic.ListView):
-   queryset = Post.objects.all()
-   template_name = "post_list.html"
+   queryset = Post.objects.all().annotate(
+       like_count=Count('likes'),
+       share_count=Count('shares')
+   )
+   template_name = "post_list.html"   
 
 
 
