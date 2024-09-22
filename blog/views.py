74536c38f67e811c17ac47d9a36a045b01cdc30d
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2024-09-22 12:49:10 +0000

    Add categories to the nav bar

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
+from django.views.generic import ListView
+from django.http import HttpResponseRedirect
 from django.db.models import Count
 from .models import Post, Like, Share
 
@@ -31,5 +33,35 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     return render( request,"blog/post_detail.html",{"post": post})   
 
+# Category list view
+class CatListView(ListView):
+    """
+     Returns all published posts in :model:`blog.Category`
+    and displays them on a page.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``category``
+        Group of published posts in :model:`blog.Category`
+    displayed on a page.
+    """
+    template_name = 'bewell_blog/category.html'
+    context_object_name = 'catlist'
+
+    def get_queryset(self):
+        content = {
+            'cat': self.kwargs['category'],
+            'posts': Post.objects.filter(category__name=self.kwargs[
+                'category']).filter(status=1)
+        }
+        return content
 
 
+def category_list(request):
+    category_list = Category.objects.exclude(name='other')
+    context = {
+        "category_list": category_list,
+    }
+    return context
