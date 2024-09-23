ee8eb5afb6668bfb854310cd9b8556ffaf0c1844
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2024-09-23 23:23:22 +0000

    add comment-edit button

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
+from django.contrib import messages
 from django.views.generic import ListView
 from django.http import HttpResponseRedirect
 from django.db.models import Count
-from .models import Post, Like, Share, Category
+from .models import Post, Like, Share, Category, Comment
 from .forms import CommentForm
 
 # Create your views here.
@@ -41,6 +42,7 @@ def post_detail(request, slug):
             comment.author = request.user
             comment.post = post
             comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment submitted and awaiting approval')
     comment_form = CommentForm()
 
     return render(request,"blog/post_detail.html",
@@ -51,6 +53,29 @@ def post_detail(request, slug):
         "comment_form": comment_form,
       },
     )
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
 class CatListView(ListView):
     """
     Displays all published posts within a specific category.
